8f9236f53ee17c01096278f8d6cd5e6ad46b4290
zmg <z20200602@outlook.com>
2022-05-20 11:24:54 +0800

    tcp server

--- demo/server/tcp.php
+++ demo/server/tcp.php
@@ -3,9 +3,17 @@
 //创建Server对象，监听 127.0.0.1:9501 端口
 $server = new Swoole\Server('127.0.0.1', 9501);
 
+$server->set(array(
+    // 'reactor_num'   => 2,     // reactor 线程数
+    'worker_num'    => 4,     // worker 进程数 =》 cup核数的1-4倍
+    'backlog'       => 128,   // listen backlog
+    // 'max_request'   => 50,
+    // 'dispatch_mode' => 1,
+));
+
 //监听连接进入事件
-$server->on('Connect', function ($server, $fd) {
-    echo "Client-$fd: Connect.\n";
+$server->on('Connect', function ($server, $fd, $reactor_id) {
+    echo "Client- {$fd} - {$reactor_id}: Connect.\n";
 });
 
 //监听数据接收事件
