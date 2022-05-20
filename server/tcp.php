943639746aa44134d08e47ca3f0ace7e08b95183
zmg <z20200602@outlook.com>
2022-05-20 11:14:49 +0800

    tcp server

--- server/tcp.php
+++ server/tcp.php
@@ -1,3 +1,23 @@
 <?php
 
-phpinfo();
+//创建Server对象，监听 127.0.0.1:9501 端口
+$server = new Swoole\Server('127.0.0.1', 9501);
+
+//监听连接进入事件
+$server->on('Connect', function ($server, $fd) {
+    echo "Client-$fd: Connect.\n";
+});
+
+//监听数据接收事件
+$server->on('Receive', function ($server, $fd, $reactor_id, $data) {
+    $server->send($fd, "Server: {$data}");
+});
+
+//监听连接关闭事件
+$server->on('Close', function ($server, $fd) {
+    echo "Client-$fd: Close.\n";
+});
+
+//启动服务器
+$server->start(); 
+
