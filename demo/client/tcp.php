dc559385ba8697253c918a8895c8701c6fa4ba86
zmg <z20200602@outlook.com>
2022-05-20 11:53:26 +0800

    tcp server

---
+++ demo/client/tcp.php
@@ -0,0 +1,16 @@
+<?php
+
+$client = new Client(SWOOLE_SOCK_TCP);
+
+if (!$client->connect('127.0.0.1', 9501, 0.5)) exit('链接失败！');
+
+// php cli常量
+fwrite(STDOUT, "请输入内容:");
+$msg = trim(fgets(STDIN));
+
+// 给tcp服务端发送数据
+$client->send($msg);
+// 接收tcp服务端数据
+echo $client->recv();
+// 关闭客户端
+$client->close();
