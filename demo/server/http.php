f66967463357ac9902f40473d04fce6317081114
zmg <z20200602@outlook.com>
2022-05-23 11:35:45 +0800

    http server

--- demo/server/http.php
+++ demo/server/http.php
@@ -2,10 +2,22 @@
 
     $http = new Swoole\Http\Server('0.0.0.0', 9501);
 
+    $http->set([
+        'enable_static_handler' => true,
+        'document_root' => '/www/wwwroot/swoole/swoole/public',
+    ]);
     $http->on('Request', function ($request, $response) {
+        if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
+            $response->end();
+            return;
+        }
         $response->header('Content-Type', 'text/html; charset=utf-8');
         $response->cookie('address', 'xiamen', time()+60*10);
-        $response->end('sss. #' . json_encode($request->get) . rand(1000,9999));
+        // $response->end('sss. #' . json_encode($request->get) . rand(1000,9999));
+
+        list($controller, $action) = explode('/', trim($request->server['request_uri'], '/'));
+        //根据 $controller, $action 映射到不同的控制器类和方法
+        (new $controller)->$action($request, $response);
     });
 
     $http->start();
