84f0ce7ad12b6066be99614337a8e7fc8817c8af
zmg <z20200602@outlook.com>
2022-05-23 17:11:20 +0800

    timer

--- demo/server/timer.php
+++ demo/server/timer.php
@@ -7,6 +7,7 @@ $ws = new Swoole\WebSocket\Server('0.0.0.0', 9502);
 $ws->on('Open', function ($ws, $request) {
     $ws->push($request->fd, "hello, welcome\n");
     if ($request->fd == 1) {
+        //设置一个间隔时钟定时器。
         Swoole\Timer::tick(2000, function($timerId) {
             echo "2s: timerId:{$timerId}\n";
         });
@@ -16,7 +17,7 @@ $ws->on('Open', function ($ws, $request) {
 //监听WebSocket消息事件
 $ws->on('Message', function ($ws, $frame) {
     echo "Message: {$frame->data}\n";
-
+    //在指定的时间后执行函数。
     Swoole\Timer::after(5000, function() use ($ws, $frame) {
         echo "5s-after\n";
         $ws->push($frame->fd, "server time after: ".date('Y-m-d H:i:s'));
