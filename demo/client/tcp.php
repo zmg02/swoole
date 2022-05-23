65f499eab4f60a00f11acc6f0d16ddc0346cd368
zmg <z20200602@outlook.com>
2022-05-23 10:36:45 +0800

    client

--- demo/client/tcp.php
+++ demo/client/tcp.php
@@ -1,17 +1,24 @@
 <?php
 
-$client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
+use Swoole\Coroutine\Client;
+use function Swoole\Coroutine\run;
 
-if (!$client->connect('127.0.0.1', 9501)) exit('连接失败！');
+run(function () {
+    $client = new Client(SWOOLE_SOCK_TCP);
+    if (!$client->connect('127.0.0.1', 9501, 0.5))
+    {
+        echo "连接失败. Error: {$client->errCode}\n";
+    }
 
-// php cli常量
-fwrite(STDOUT, "请输入内容:");
-$msg = trim(fgets(STDIN));
+    // php cli常量
+    fwrite(STDOUT, "请输入内容:");
+    $msg = trim(fgets(STDIN));
 
-// 给tcp服务端发送数据
-$client->send($msg);
-// 接收tcp服务端数据
-echo $client->recv();
-// 关闭客户端
-$client->close();
+    // 给tcp服务端发送数据
+    $client->send("$msg\n");
+    // 接收tcp服务端数据
+    echo $client->recv();
+    // 关闭客户端
+    $client->close();
+});
 
