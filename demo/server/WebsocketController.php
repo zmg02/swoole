98e569a76a85fc1ab8358d76732a997fc0f3d613
zmg <z20200602@outlook.com>
2022-05-23 16:00:56 +0800

    websocket

--- demo/server/WebsocketController.php
+++ demo/server/WebsocketController.php
@@ -13,10 +13,13 @@ class WebsocketController
         $this->ws->set([
             'enable_static_handler' => true,
             'document_root' => '/www/wwwroot/swoole/swoole/public',
+            'task_worker_num' => 4,
         ]);
 
         $this->ws->on('open', [$this, 'onOpen']);
         $this->ws->on('message', [$this, 'onMessage']);
+        $this->ws->on('task', [$this, 'onTask']);
+        $this->ws->on('finish', [$this, 'onFinish']);
         $this->ws->on('close', [$this, 'onClose']);
         $this->ws->start();
     }
@@ -39,7 +42,41 @@ class WebsocketController
     public function onMessage($ws, $frame)
     {
         echo "Message: {$frame->data}\n";
+        // todo 5s
+        $data = [
+            'task' => 1,
+            'fd'   => $frame->fd,
+        ];
+        $ws->task($data);
         $ws->push($frame->fd, "Server: {$frame->data}\n");
+    }
+
+    /**
+     * 处理异步任务（Task）
+     * @param [type] $ws
+     * @param [type] $taskId
+     * @param [type] $reactorId
+     * @param [type] $data
+     * @return void
+     */
+    public function onTask($ws, $taskId, $reactorId, $data)
+    {
+        echo "New AsyncTsak[id={$taskId}]".PHP_EOL;
+        // 耗时场景 5s
+        sleep(5);
+        return json_encode($data);
+    }
+
+    /**
+     * 处理异步任务的结果
+     * @param [type] $ws
+     * @param [type] $taskId
+     * @param [type] $data
+     * @return void
+     */
+    public function onFinish($ws, $taskId, $data)
+    {
+        echo "AsyncTask[{$taskId}] Finish: {$data}".PHP_EOL;
     }
 
     /**
