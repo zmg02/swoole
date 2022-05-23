22d7c5777a42443f1fb244f14cab3a349c0c53fc
zmg <z20200602@outlook.com>
2022-05-23 17:08:09 +0800

    http

--- demo/server/http.php
+++ demo/server/http.php
@@ -6,7 +6,7 @@
         'enable_static_handler' => true,
         'document_root' => '/www/wwwroot/swoole/swoole/public',
     ]);
-    
+
     $http->on('Request', function ($request, $response) {
         if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
             $response->end();
@@ -16,12 +16,13 @@
         $response->cookie('address', 'xiamen', time()+60*10);
         // $response->end('sss. #' . json_encode($request->get) . rand(1000,9999));
 
-        list($controller, $action) = explode('/', trim($request->server['request_uri'], '/'));
-        //根据 $controller, $action 映射到不同的控制器类和方法
-        $controllerName = $controller.'Controller';
-        $controllerPath = $controllerName.'.php';
-        include_once $controllerPath;
-        $result = (new $controllerName)->$action($request, $response);
+        // list($controller, $action) = explode('/', trim($request->server['request_uri'], '/'));
+        // //根据 $controller, $action 映射到不同的控制器类和方法
+        // $controllerName = $controller.'Controller';
+        // $controllerPath = $controllerName.'.php';
+        // include_once $controllerPath;
+        // $result = (new $controllerName)->$action($request, $response);
+        $result = 'http server';
         $response->end('result: ' . $result . ' # ' . rand(1000,9999));
     });
 
