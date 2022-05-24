8c135ef1ff60e595f0d9a8da9dd86db6be45061f
zmg <z20200602@outlook.com>
2022-05-24 11:15:54 +0800

    write

--- demo/io/write.php
+++ demo/io/write.php
@@ -5,5 +5,7 @@ $fileName = __DIR__ . '/data/mysql.txt';
 Swoole\Coroutine\run(function() use ($fileName) {
     $content = 'varchar与char的区别？'.PHP_EOL;
     $w = Swoole\Coroutine\System::writeFile($fileName, $content, FILE_APPEND);
-    var_dump($w);
+    echo 'success'.PHP_EOL;
 });
+
+echo 'start'.PHP_EOL;
