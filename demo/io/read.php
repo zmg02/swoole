ae4a6e19e5ad2722ea830789e2c437a4cfac7581
zmg <z20200602@outlook.com>
2022-05-24 10:46:39 +0800

    io

--- demo/io/read.php
+++ demo/io/read.php
@@ -5,3 +5,8 @@ Swoole\Coroutine\run(function () use ($fileName) {
     $r = Swoole\Coroutine\System::readFile($fileName);
     var_dump($r);
 });
+
+$res = Swoole\Coroutine\System::readFile($fileName, function($fileName, $fileContent) {
+    echo 'filename:'.$fileName.PHP_EOL;
+});
+var_dump($res);
